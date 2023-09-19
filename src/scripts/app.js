c46ede503e8a0a2b0000bec0c6022b77f906e109
floppy3 <flolambert@live.fr>
2023-09-20 00:26:06 +0200

    je sai splus

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,15 @@
-console.info('Hello world');
+let menuBurger = document.querySelector('.menu__burger');
+menuBurger.addEventListener('click',toggleNavigation);
+let menuSpan = document.querySelector('.menu__burger span');
+let navLinks = document.querySelectorAll('.menu__el a');
+navLinks.forEach(link => link.addEventListener('click', toggleNavigation));
+
+function toggleNavigation(){
+    if(document.body.hasAttribute("data-menu")){
+        document.body.removeAttribute("data-menu");
+        menuSpan.innerHTML = "Menu";
+    }else{
+        document.body.setAttribute("data-menu", true);
+        menuSpan.innerHTML = "Close";
+    }
+}
