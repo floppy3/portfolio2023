a907fa3c3bb8f64c9c310fb5fef10d539bc50c5f
floppy3 <flolambert@live.fr>
2023-09-24 20:30:42 +0200

    menu data presponsive

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,24 @@
-let menuBurger = document.querySelector('.menu__burger');
-menuBurger.addEventListener('click',toggleNavigation);
-let menuSpan = document.querySelector('.menu__burger span');
-let navLinks = document.querySelectorAll('.menu__el a');
-navLinks.forEach(link => link.addEventListener('click', toggleNavigation));
+"use strict";
 
-function toggleNavigation(){
-    if(document.body.hasAttribute("data-menu")){
-        document.body.removeAttribute("data-menu");
-        menuSpan.innerHTML = "Menu";
-        document.body.classList.remove("disable-scrolling");
-    }else{
-        document.body.setAttribute("data-menu", true);
-        menuSpan.innerHTML = "Close";
-        document.body.classList.add("disable-scrolling")
+let width = screen.width;
+
+if (width < 1024){
+    console.log(width);
+    let menuBurger = document.querySelector('.menu__burger');
+    menuBurger.addEventListener('click',toggleNavigation);
+    let menuSpan = document.querySelector('.menu__burger span');
+    let navLinks = document.querySelectorAll('.menu__el a');
+    navLinks.forEach(link => link.addEventListener('click', toggleNavigation));
+
+    function toggleNavigation(){
+        if(document.body.hasAttribute("data-menu")){
+            document.body.removeAttribute("data-menu");
+            menuSpan.innerHTML = "Menu";
+            document.body.classList.remove("disable-scrolling");
+        }else{
+            document.body.setAttribute("data-menu", true);
+            menuSpan.innerHTML = "Close";
+            document.body.classList.add("disable-scrolling")
+        }
     }
 }
